42fd75448edeac0cb3d9398a4a94ac70d11cae07
Nathan Seymour <nathan@seymour.global>
2022-10-07 14:31:50 +0100

    version 0.0.4

--- generated-api/models/index.ts
+++ generated-api/models/index.ts
@@ -1,7 +1,10 @@
 /* tslint:disable */
 /* eslint-disable */
 export * from './Account';
+export * from './Book';
+export * from './BookCategoriesInner';
 export * from './Credentials';
+export * from './GetBookRequest';
 export * from './LoginResponse';
 export * from './ResetPasswordRequest';
 export * from './UpdateAccountByEmailRequest';
